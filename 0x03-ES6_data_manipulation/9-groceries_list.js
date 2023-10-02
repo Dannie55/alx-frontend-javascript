21f8d9601e15cd4051cb9d2dce0a4c43c458e02b
Dannie55 <ezirimdaniel080@gmail.com>
2023-10-02 19:06:04 +0000

    map data structure

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,11 @@
 export default function groceriesList(){
-  const groceryMap = new Map([
+  const groceries =[
         ["Apples", 10],
         ["Tomatoes", 10],
         ["Pasta", 1],
         ["Rice", 1],
         ["Banana", 5]
-    ]);
+    ];
+    const groceryMap = new Map(groceries);
     return groceryMap;
 };
